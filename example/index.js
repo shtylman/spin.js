00c03d04c0cde9c17b352b6d62170b53da1ffc97
Roman Shtylman <shtylman@gmail.com>
2013-01-25 14:30:43 -0500

    make example "interactive" enabled

---
+++ example/index.js
@@ -0,0 +1,34 @@
+var Spinner = require('spin');
+
+// default spinner
+var target = document.createElement('div');
+new Spinner().spin(target);
+
+target;
+// =>
+
+// spinners can be configured with any number of options
+// defaults, change what you want
+var opt = {
+    lines: 13, // The number of lines to draw
+    length: 7, // The length of each line
+    width: 4, // The line thickness
+    radius: 10, // The radius of the inner circle
+    corners: 1, // Corner roundness (0..1)
+    rotate: 0, // The rotation offset
+    color: '#000', // #rgb or #rrggbb
+    speed: 1, // Rounds per second
+    trail: 60, // Afterglow percentage
+    shadow: false, // Whether to render a shadow
+    hwaccel: false, // Whether to use hardware acceleration
+    className: 'spinner', // The CSS class to assign to the spinner
+    zIndex: 2e9, // The z-index (defaults to 2000000000)
+    top: 'auto', // Top position relative to parent in px
+    left: 'auto' // Left position relative to parent in px
+};
+
+var target = document.createElement('div');
+new Spinner(opt).spin(target);
+
+target;
+// =>
